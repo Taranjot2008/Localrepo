2dd06e5f77b9ec71d27baca7a852a14593927833
Taranjot2008 <taranjotscience@gmail.com>
2024-07-24 23:18:12 -0300

    First app

--- practice.py
+++ practice.py
@@ -1,7 +1,7 @@
-from PIL import Image
-import requests
-import streamlit as st
-from streamlit_lottie import st_lottie
+from PIL import Image #type: ignore
+import requests # type: ignore
+import streamlit as st # type: ignore
+from streamlit_lottie import st_lottie #type: ignore
 
 st.set_page_config(page_title='My Webpage',page_icon=':tada:',layout='wide')
 
